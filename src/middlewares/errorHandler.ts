b7328f7fe924dbf567aad360092b24a43b7eca10
오윤 <passfile2@naver.com>
2025-12-29 15:20:00 +0900

    ✨ feat: 에러핸들러 구현 및 전역 미들웨어 등록

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,35 @@
+import { Prisma } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+
+export function defaultNotFoundHandler(req: Request, res: Response, next: NextFunction) {
+  return res.status(404).send({ message: '잘못된 접근입니다.' });
+}
+
+export function globalErrorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  console.log(err);
+  // 1️⃣ Prisma known error 처리
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    return res.status(400).json({
+      type: 'PrismaClientKnownRequestError',
+      code: err.code,
+      message: err.message,
+    });
+  }
+
+  // 2️⃣ Zod validation error 처리
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      type: 'ZodError',
+      issues: err.issues,
+      message: err.message,
+    });
+  }
+
+  // 3️⃣ 기타 에러 처리
+  const statusCode = err.status || 500;
+  return res.status(statusCode).json({
+    type: 'UnknownError',
+    message: err.message || 'Internal Server Error',
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { PORT } from './libs/constants';
 import cors from 'cors';
 import authRouter from './modules/auth/auth.router';
+import { defaultNotFoundHandler, globalErrorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
@@ -9,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/auth', authRouter);
+
+app.use(defaultNotFoundHandler);
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => console.log('Server Running'));
