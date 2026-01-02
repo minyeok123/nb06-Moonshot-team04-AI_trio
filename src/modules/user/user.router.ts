88decf22514d60063f153d93e623d4a20314938b
kimjs <kimjs910522@gmail.com>
2026-01-02 13:52:33 +0900

    ♻️ refactor: User 파트 전체 토큰 검증 추가

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -2,20 +2,22 @@ import express from 'express';
 import { UserController } from './user.controller';
 import authenticate from '../../middlewares/authenticate';
 import asyncHandler from '../../libs/asyncHandler';
-import { validate } from '../../middlewares/validate';
+import { validate, tokenValidate } from '../../middlewares/validate';
 import { userPasswordValidator, getProjectListValidator } from './user.validator';
 
 const router = express.Router();
 
-router.get('/me', authenticate, asyncHandler(UserController.userInfo));
+router.get('/me', tokenValidate(), authenticate, asyncHandler(UserController.userInfo));
 router.post(
   '/me',
+  tokenValidate(),
   authenticate,
   validate(userPasswordValidator, 'body'),
   asyncHandler(UserController.userInfoChange),
 );
 router.get(
   '/me/projects',
+  tokenValidate(),
   authenticate,
   validate(getProjectListValidator, 'query'),
   asyncHandler(UserController.userJoinProjects),
