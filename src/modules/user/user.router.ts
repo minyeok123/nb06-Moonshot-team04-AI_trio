c2e80a599211b4b4cc8cc9ef22249f18adebfec9
kimjs <kimjs910522@gmail.com>
2026-01-02 13:19:39 +0900

    ✨ feat: USER - Get 참여중인 프로젝트 조회 기능 구현

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -3,8 +3,8 @@ import { UserController } from './user.controller';
 import authenticate from '../../middlewares/authenticate';
 import asyncHandler from '../../libs/asyncHandler';
 import { validate } from '../../middlewares/validate';
-import { userPasswordValidator } from './user.validator';
 import upload from '../../middlewares/upload';
+import { userPasswordValidator, getProjectListValidator } from './user.validator';
 
 const router = express.Router();
 
@@ -12,9 +12,15 @@ router.get('/me', authenticate, asyncHandler(UserController.userInfo));
 router.post(
   '/me',
   authenticate,
-  upload.single('profileImg'),
+  upload.single('profileImage'),
   validate(userPasswordValidator, 'body'),
   asyncHandler(UserController.userInfoChange),
+);
+router.get(
+  '/me/projects',
+  authenticate,
+  validate(getProjectListValidator, 'query'),
+  asyncHandler(UserController.userJoinProjects),
 );
 
 export default router;
--- src/modules/user/user.validator.ts
+++ src/modules/user/user.validator.ts
@@ -7,3 +7,10 @@ export const userPasswordValidator = z.object({
   newPassword: z.string().min(8).max(16),
   checkNewPassword: z.string().min(8).max(16),
 });
+
+export const getProjectListValidator = z.object({
+  page: z.coerce.number().int().min(1).default(1),
+  limit: z.coerce.number().int().min(1).max(50).default(10),
+  order: z.enum(['asc', 'desc']).default('desc'),
+  order_by: z.enum(['created_at', 'name']).default('created_at'),
+});
