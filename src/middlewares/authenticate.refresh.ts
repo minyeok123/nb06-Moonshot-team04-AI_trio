7f32103d6729ef856103bf679f64e2b5814d19ce
Max <jeon@kakao.com>
2026-01-08 17:35:47 +0900

    refactor: 해더 추출 함수 분리

--- src/middlewares/authenticate.refresh.ts
+++ src/middlewares/authenticate.refresh.ts
@@ -5,18 +5,35 @@ import { compareData } from '../modules/auth/utils/hash';
 import Token from '../modules/auth/utils/token';
 
 export const authenticateRefresh = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+  const parseTokenFromHeader = (req: Request) => {
     const header = req.headers.authorization;
-    if (!header) throw new CustomError(401, '토큰이 없습니다');
+    if (!header) {
+      throw new CustomError(401, '토큰이 없습니다');
+    }
+
     const token = header?.split(' ')[1];
+    return token;
+  };
+
+  try {
+    const token = parseTokenFromHeader(req);
     const payload = Token.verifyRefreshToken(token);
-    if (!payload) throw new CustomError(401, '토큰 만료');
+    if (!payload) {
+      throw new CustomError(401, '토큰 만료');
+    }
+
     const savedRefreshToken = await prisma.refreshToken.findFirst({
       where: { userId: payload.id },
     });
-    if (!savedRefreshToken) throw new CustomError(401, '토큰이 없습니다');
+    if (!savedRefreshToken) {
+      throw new CustomError(401, '토큰이 없습니다');
+    }
+
     const compareRefresh = await compareData(token, savedRefreshToken.token);
-    if (!compareRefresh) throw new CustomError(401, '잘못된 접근입니다');
+    if (!compareRefresh) {
+      throw new CustomError(401, '잘못된 접근입니다');
+    }
+
     req.refresh = { id: payload.id };
     next();
   } catch (e) {
