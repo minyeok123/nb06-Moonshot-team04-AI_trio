51f0cd0ff04c75de5d60f965aff28c30e02cab0d
kimjs <kimjs910522@gmail.com>
2026-01-13 14:17:06 +0900

    🐛 fix: 구글 로그인 이미지 저장/연동 수정

--- src/modules/file/file.service.ts
+++ src/modules/file/file.service.ts
@@ -1,15 +1,11 @@
 import { BASE_URL } from '@libs/constants';
 
 export class FileService {
-  createFileUrl = (file: Express.Multer.File) => {
-    if (file) {
-      const imgUrl = `${BASE_URL}/uploads/${file.filename}`;
-      return imgUrl;
+  createFileUrl = (file: string | Express.Multer.File) => {
+    if (typeof file === 'string') {
+      return file.startsWith('http') ? file : `${BASE_URL}${file}`;
     }
 
-    // if (Array.isArray(file)) {
-    //   const imgUrls = file.map((f: any) => `/uploads/${f.filename}`);
-    //   return imgUrls;
-    // }
+    return `${BASE_URL}/uploads/${file.filename}`;
   };
 }
