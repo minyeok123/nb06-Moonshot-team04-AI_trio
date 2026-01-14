435e1ce610ccf7f72caac3bfee25e66ac79b3866
minyeok <rltkeh150@naver.com>
2026-01-14 12:03:29 +0900

    🐛 fix:쿠키설정 변경9

--- src/middlewares/googleAuth.ts
+++ src/middlewares/googleAuth.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { CustomError } from '@libs/error';
 
 export async function verifyState(req: Request, res: Response, next: NextFunction) {
+  console.log(req.session.oauthState);
   try {
-    console.log(req.session.oauthState);
     if (req.session.oauthState !== req.query.state)
       throw new CustomError(404, '부적절한 접근이 감지되었습니다');
     delete req.session.oauthState;
